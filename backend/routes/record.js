f5605c1672094e075aa2c1a7b2f9da219d9183e5
KristaSmith1 <122482815+KristaSmith1@users.noreply.github.com>
2024-07-11 20:07:24 -0600

    Update record.js
    Changed line 21 database from "employees" to "helloWorld"

--- backend/routes/record.js
+++ backend/routes/record.js
@@ -17,7 +17,7 @@ const ObjectId = require("mongodb").ObjectId;
 // This section will help you get a list of all the records.
 recordRoutes.route("/record").get(async (req, res) => {
     try {
-        let db_connect = dbo.getDb("employees");
+        let db_connect = dbo.getDb("helloWorld");
         const result = await db_connect.collection("records").find({}).toArray();
         res.json(result);
     } catch (err) {
